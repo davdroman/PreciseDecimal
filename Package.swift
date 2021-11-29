a9ac1ea10ad4ce8d1cbf7f7ce5ebb7ff197221a5
davdroman <d@vidroman.dev>
2021-11-29 22:27:44 +0000

    Bump version requirements

--- Package.swift
+++ Package.swift
@@ -1,10 +1,16 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "PreciseDecimal",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6),
+    ],
     products: [
         .library(
             name: "PreciseDecimal",
