021193b395a8f97d2356236d921c32cb3016b6e4
David Roman <2538074+davdroman@users.noreply.github.com>
2023-09-05 16:40:17 +0100

    wip

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -32,5 +32,5 @@ let package = Package(
 )
 
 package.dependencies = [
-    .package(url: "https://github.com/davdroman/swift-json-testing", branch: "main"),
+    .package(url: "https://github.com/davdroman/swift-json-testing", from: "0.2.0"),
 ]
